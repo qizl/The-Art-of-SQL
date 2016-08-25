d4c1a4ad02b4136cac031b00ae3511151ac5a5ce
qizl <q@qizl.cn>
2016-08-25 15:55:55 +0800

    新建MVC5 项目

---
+++ src/TheArtofSQL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheArtofSQL.Startup))]
+namespace TheArtofSQL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
